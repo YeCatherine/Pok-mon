18d77ed211679f4de890bf3d57cd8307ad616d95
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-17 16:56:36 +0300

    Pokémon: added typedoc for Pokemon type

--- src/Types/Pokemon.ts
+++ src/Types/Pokemon.ts
@@ -1,10 +1,38 @@
+/**
+ * Pokemon data type.
+ */
 export default interface IPokemonData {
+    /**
+     * Unique pokemon id.
+     */
     id?: any | null,
+    /**
+     * Pokemon name.
+     */
     name: string,
+    /**
+     * Weight.
+     */
     weight: string,
+    /**
+     * Height
+     */
     height: string,
+    /**
+     * Order number
+     */
     order: string,
+    /**
+     * Pokemon types
+     */
     types: Array<string>,
+    /**
+     * Abilities
+     */
     abilities: Array<string>,
+
+    /**
+     * Evolution chain
+     */
     published?: boolean,
 }
