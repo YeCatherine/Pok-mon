e0e87cd9e4ddd99160b459de115801674d81546a
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-19 00:22:13 +0300

    Pokémon: restructured button and changed it to react element

--- src/Components/CapturedPokemons.tsx
+++ src/Components/CapturedPokemons.tsx
@@ -2,30 +2,41 @@ import React from "react";
 import PokeComponentType from "../Types/PokeComponentType";
 import useLocalStorage from "../Hooks/useLocalStorage";
 import IPokemonData from "../Types/Pokemon";
+import {ListGroup} from "react-bootstrap";
 
+interface ICaptureButton {
+    pokemon: IPokemonData;
+    checkCapturedPokemon: (pokemon: IPokemonData) => boolean;
+    setCapturePokemon: (pokemon: IPokemonData) => void;
+}
+
+const CaptureButton: React.FC<ICaptureButton> = (props) => {
+    const {pokemon, checkCapturedPokemon, setCapturePokemon} = props;
+    return (<button
+        onClick={() => setCapturePokemon(pokemon)}>
+        {checkCapturedPokemon(pokemon) ? "Escape" : "Capture"}
+    </button>);
+}
 const CapturedPokemons: React.FC<PokeComponentType> = (props) => {
     const {checkCapturedPokemon, setCapturePokemon} = props;
     const [capturedPokemonList] = useLocalStorage<Array<IPokemonData>>('capturedPokemonList', []);
     const list = capturedPokemonList.map((currentPokemon, index) =>
-        (<li key={index}>
+        (<ListGroup.Item key={index}>
             {currentPokemon.name}
-            <button
-                onClick={() => setCapturePokemon(currentPokemon)}>
-                {checkCapturedPokemon(currentPokemon) ? "Escape" : "Capture"}
-            </button>
-        </li>)
+            <CaptureButton pokemon={currentPokemon} checkCapturedPokemon={checkCapturedPokemon}
+                           setCapturePokemon={setCapturePokemon}/>
+        </ListGroup.Item>)
     );
 
     return (
         <>
             <div className="col-md-6">
                 <h3>Captured Pokemon List</h3>
-                <ul className="list-group">
+                <ListGroup className="list-group">
                     {list}
-                </ul>
+                </ListGroup>
             </div>
         </>
     );
-
 }
 export default CapturedPokemons;
