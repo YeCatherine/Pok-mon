f789c3651fc85ba9497d63942d94ec88c65f2ccb
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-18 10:40:28 +0300

    Pokémon: restructured pokemon list page

--- src/Components/PokemonList.tsx
+++ src/Components/PokemonList.tsx
@@ -2,19 +2,14 @@ import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import PokemonListService from "../Services/PokemonListService";
 import IPokemonData from "../Types/Pokemon";
-import useLocalStorage from "../Hooks/useLocalStorage";
+import CapturedPokemons from './CapturedPokemons';
+import PokeComponentType from "../Types/PokeComponentType";
 
-const PokemonList: React.FC = () => {
-
+const PokemonList: React.FC<PokeComponentType> = (props) => {
+    const {checkCapturedPokemon, setCapturePokemon} = props;
     const [pokemon, setPokemon] = useState<Array<IPokemonData>>([]);
-    const [currentIndex, setCurrentIndex] = useState(-1);
-    const [capturedPokemonList, setCapturedPokemonList] = useLocalStorage<Array<IPokemonData>>('capturedPokemonList', []);
 
     useEffect(() => {
-        retrievePokemon();
-    }, []);
-
-    const retrievePokemon = () => {
         PokemonListService.getAll()
             .then((response: any) => {
                 setPokemon(response.data.results);
@@ -23,16 +18,7 @@ const PokemonList: React.FC = () => {
             .catch((e: any) => {
                 console.log(e);
             })
-    };
-
-    const setCapturePokemon = (pokemon: IPokemonData, index: number) => {
-        if (capturedPokemonList.indexOf(pokemon) !== null) {
-
-            console.log(capturedPokemonList.indexOf(pokemon));
-
-            setCapturedPokemonList([...capturedPokemonList, pokemon])
-        }
-    };
+    }, []);
 
     return (
         <div className="list-row">
@@ -42,40 +28,20 @@ const PokemonList: React.FC = () => {
                     <li>
                         <Link to="/">Home</Link>
                     </li>
-                    {pokemon && pokemon.map((onePokemon, index) => {
-                        const name = onePokemon.name;
-                        return (
-                            <li className={"list-group-item" + (index === currentIndex ? "active" : "")}
-                                onClick={() => setCapturePokemon(onePokemon, index)}
-                                key={index}
-                            >
-                                <Link to={`/pokemon/${name}`}>{name}</Link>
-                                <button>Captured</button>
-                            </li>
-                        )
-                    })}
+                    {pokemon && pokemon.map((onePokemon, index) =>
+                        (<li key={index}>
+                            <Link to={`/pokemon/${onePokemon.name}`}>{onePokemon.name}</Link>
+                            <button
+                                onClick={() => setCapturePokemon(onePokemon)}>
+                                {checkCapturedPokemon(onePokemon) ? "Escape" : "Capture"}
+                            </button>
+                        </li>)
+                    )}
                 </ul>
             </div>
-
-            <div className="col-md-6">
-                <h3>Captured Pokemon List</h3>
-                <ul className="list-group">
-                    {capturedPokemonList && capturedPokemonList.map((onePokemon, index) => {
-                        const name = onePokemon.name;
-                        return (
-                            <li className={"list-group-item" + (index === currentIndex ? "active" : "")}
-                                onClick={() => setCapturePokemon(onePokemon, index)}
-                                key={index}
-                            >
-                                {name}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
+            <CapturedPokemons checkCapturedPokemon={checkCapturedPokemon} setCapturePokemon={setCapturePokemon}/>
         </div>
     )
-
 }
 
 export default PokemonList;
