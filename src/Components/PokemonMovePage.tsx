990ad5445bb7b1cbdf3f300ec290ec3d6fa641a3
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-19 00:27:34 +0300

    Pokémon: added bootstrap to pokemon move page

--- src/Components/PokemonMovePage.tsx
+++ src/Components/PokemonMovePage.tsx
@@ -1,9 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {Link, useParams} from "react-router-dom";
-
 import IPokemonData from "../Types/Pokemon";
-import PokeComponentType from "../Types/PokeComponentType";
 import PokemonListService from "../Services/PokemonListService";
+import {Container, ListGroup as Ul, ListGroupItem as Li} from "react-bootstrap";
 
 const PokemonMovePage: React.FC = (props) => {
     const params = useParams<any>();
@@ -25,13 +24,13 @@ const PokemonMovePage: React.FC = (props) => {
         console.log(props);
         return <>
             <h1>Related Pokemon</h1>
-            <ul>
-                {props?.move?.learned_by_pokemon.map(pokemon => <li>{pokemon.name}</li>)}
-            </ul>
+            <Container>
+                <Ul>
+                    {props?.move?.learned_by_pokemon.map(pokemon => <Li>{pokemon.name}</Li>)}
+                </Ul>
+            </Container>
         </>
-
     };
-
 
     return (<>
         <h1>Move name "{params.name}"</h1>
