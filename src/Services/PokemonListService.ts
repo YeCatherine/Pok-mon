c89d71036036c5758d757f2594fbe5a8c58f4c77
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-19 12:50:11 +0300

    Pokémon: added documentation to Types

--- src/Services/PokemonListService.ts
+++ src/Services/PokemonListService.ts
@@ -1,15 +1,32 @@
 import http from "../http-common";
 
-//@todo change limit after dev.
+/**
+ * Retrieves the Get request from "/pokemon?limit= ".
+ */
 const getAll = (): any => {
+    //@todo change limit after dev.
     return http.get("/pokemon?limit=10");
 }
+
+/**
+ * Retrieves the Get request from `/pokemon/${name}`.
+ * @param name The name of pokemon.
+ */
 const get = (name: string): any => {
     return http.get(`/pokemon/${name}`);
 }
+
+/**
+ * Retrieves the Get request from `/move/${name}`.
+ * @param name The name of pokemon.
+ */
 const getMove = (name: string): any => {
     return http.get(`/move/${name}`);
 }
+
+/**
+ * Retrieves the Get request from separate endpoint.
+ */
 const PokemonListService = {
     getAll,
     get,
