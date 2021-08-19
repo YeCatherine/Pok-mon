39f78b0af71bdf01636b15f61b3161b369739511
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-19 12:56:21 +0300

    Pokémon: added documentation and renewed readme

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+/**
+ * Uses the base url and headers for fetching data.
+ */
 export default axios.create({
     baseURL: "https://pokeapi.co/api/v2/",
     headers: {
