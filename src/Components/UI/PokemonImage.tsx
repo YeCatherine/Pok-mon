1086bf01d783820d0533d838007afa91e84ff52a
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-22 17:24:43 +0300

    Pokémon: added fallbacks to the images

--- src/Components/UI/PokemonImage.tsx
+++ src/Components/UI/PokemonImage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Image} from "react-bootstrap";
 import {getIdFromURL} from "../../Services/Common";
 import IPokemonSimpleComponent from "../../Types/IPokemonSimpleComponent";
+import ReactImageFallback from 'react-image-fallback';
 
 /**
  * Gets the image of the pokemon.
@@ -9,9 +10,16 @@ import IPokemonSimpleComponent from "../../Types/IPokemonSimpleComponent";
  * @constructor The functional component for PokemonImage.
  */
 const PokemonImage: React.FC<IPokemonSimpleComponent> = (props) => {
+    const {pokemon} = props;
     const id = getIdFromURL(props.pokemon.url);
-    const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-    return <Image alt={props.pokemon.name} src={imgUrl} roundedCircle/>
+    const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+    const fallbackImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    return <ReactImageFallback
+        src={imgUrl}
+        fallbackImage={fallbackImgUrl}
+        initialImage="loader.gif"
+        alt={pokemon.name}
+        className="pokemon-main-image"/>
 }
 
 export default PokemonImage;
