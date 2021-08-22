67fffafba467dddfbf982151ec65a159ad5eb2a1
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-23 00:33:53 +0300

    Pokémon: refactored tests

--- src/RightSidebar.test.tsx
+++ src/RightSidebar.test.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-
-import App from './App';
+import RightSidebar from "./Components/Layout/RightSidebar";
 
 describe('Find element by text', () => {
     it('Check for component "Captured"', () => {
-        render(<App/>);
+        render(<RightSidebar/>);
         const linkElement = screen.getByText(/Captured/i);
-        expect(linkElement).toBeInTheDocument();
-    });
-
-    it('Check for component "See also"', () => {
-        render(<App/>);
-        const linkElement = screen.getByText(/See also/i);
         expect(linkElement).toBeInTheDocument();
     });
 });
