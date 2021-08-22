586cf7d4fa0e74bd5567f994400775202be6f8c1
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-22 15:47:37 +0300

    Pokémon: restructured footer

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
 
-const Footer:React.FC = () => {
+const Footer: React.FC = () => {
     return (
-        <h1>Bottom</h1>
-    )
+        <Container
+            className="navbar navbar-expand-md navbar-light bg-opacity-25 bg-dark fixed-bottom">
+
+            <Row>
+                <Col>Bottom</Col>
+                <Col>
+                    <a href={"https://gist.github.com/xcambar/c6438e7330584e42f71f2a51b8b367cc"}
+                       className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">
+                        Pokédex Mission
+                    </a>
+                </Col>
+            </Row>
+        </Container>
+    );
+
 }
 export default Footer;
