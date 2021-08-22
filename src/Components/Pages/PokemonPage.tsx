04701bb954c7eca330fc01b190113144367d8a8e
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-22 18:49:19 +0300

    Pokémon: restructured pokemon page

--- src/Components/Pages/PokemonPage.tsx
+++ src/Components/Pages/PokemonPage.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {Link, useParams} from "react-router-dom";
 import IPokemonData from "../../Types/IPokemonData";
+import IPokemonSimpleComponent from "../../Types/IPokemonSimpleComponent";
 import PokemonListService from "../../Services/PokemonListService";
 import {
     Container,
@@ -10,6 +11,7 @@ import {
 } from "react-bootstrap";
 import EvolutionChain from "../UI/EvolutionChain";
 import {useGlobalContext} from '../../Services/Context'
+import PokemonCard from "../UI/PokemonCard";
 
 /**
  * Outputs the list of pokemon weight, height, order, type name, abilities, moves.
@@ -19,6 +21,7 @@ import {useGlobalContext} from '../../Services/Context'
 const PokemonPage: React.FC = (props) => {
     const params = useParams<any>();
     const [pokemon, setPokemon] = useState<IPokemonData>();
+    const [pokemonSpecies, setPokemonSpecies] = useState<IPokemonData>();
     const [name, setName] = useState<string>("");
     const {language} = useGlobalContext()
 
@@ -30,91 +33,82 @@ const PokemonPage: React.FC = (props) => {
             .catch((e: any) => {
                 console.log(e);
             });
-    }, [params.name]);
 
-    PokemonListService.getSpecies(params.name)
-        .then((response: any) => {
-            const translatedName = response.data.names.filter(lang => lang.language.name === language)
-            setName(translatedName[0].name);
-        }).catch((e: any) => {
-        console.log(e);
-    });
+        PokemonListService.getSpecies(params.name)
+            .then((response: any) => {
+                setPokemonSpecies(response.data);
+                const translatedName = response.data.names.filter(lang => lang.language.name === language)
+                setName((translatedName[0].name) ? translatedName[0].name : params.name);
+            }).catch((e: any) => {
+            console.log(e);
+        });
+    }, []);
 
-    /**
-     * Outputs the pokemon properties.
-     * @param props The pokemon.
-     * @constructor Pokemon properties.
-     */
-    const Pokemon = (props) => {
-        return <>
-            <Container>
-                <div
-                    className="list-group d-flex flex-wrap flex-row justify-content-around">
-                    <Ul>
-                        <Li className="pokemon">
-                            {`My name is "${name}"`}
-                            <ul>
-                                <Image
-                                    src={props?.pokemon?.sprites?.front_default}
-                                    alt={props?.pokemon?.name}
-                                    roundedCircle/>
-                            </ul>
-                        </Li>
-                    </Ul>
-                    <Ul><Li><EvolutionChain pokemon={props?.pokemon}/></Li></Ul>
-                    <Ul>
-                        <Li>Base Stats
-                            <ul>
-                                {props?.pokemon?.stats.map(st => (
-                                    <Li key={st.stat.name}>{st.stat.name} - {st.base_stat}</Li>))}
-                            </ul>
-                        </Li>
-                    </Ul>
-                    <Ul>
-                        <Li>My Type
-                            <ul>
-                                {props?.pokemon?.types.map(typ => (
-                                    <Li key={typ.type.name}>{typ.type.name}</Li>))}
-                            </ul>
-                        </Li>
-                    </Ul>
-                    <Ul>
-                        <Li>My Size
-                            <ul>
-                                <Li>{`weight ${props?.pokemon?.weight}`}</Li>
-                                <Li>{`height ${props?.pokemon?.height}`}</Li>
-                                <Li>{`order ${props?.pokemon?.order}`}</Li>
-                            </ul>
-                        </Li>
-                    </Ul>
-                    <Ul>
-                        <Li>My Abilities
-                            <ul>
-                                {props?.pokemon?.abilities.map(ability => (
-                                    <Li key={ability.ability.name}>{ability.ability.name}</Li>))}
-                            </ul>
-                        </Li>
-                    </Ul>
-                    <Ul>
-                        <Li>My Moves
-                            <ul>
-                                {props?.pokemon?.moves.map(move => (
-                                    <Li key={move.move.name}>
-                                        <Link
-                                            to={`/move/${move.move.name}`}>{move.move.name}</Link>
-                                    </Li>))}
-                            </ul>
-                        </Li>
-                    </Ul>
-                </div>
-            </Container>
-        </>
-    };
 
-    return (
-        <>
-            {pokemon && <Pokemon key={pokemon?.name} pokemon={pokemon}/>}
-        </>);
+    if (typeof pokemon === 'undefined' || typeof pokemonSpecies === 'undefined') return (<h1>Loading...</h1>);
+
+    return (<>
+        <Container>
+            <div
+                className="list-group d-flex flex-wrap flex-row justify-content-around">
+                <Ul>
+                    <Li className="pokemon">
+                        {`My name is "${name}"`}
+                        <Ul>
+                            <PokemonCard pokemon={pokemon}/>
+                        </Ul>
+                    </Li>
+                </Ul>
+                <Ul><Li><EvolutionChain pokemon={pokemon}
+                                        pokemonSpecies={pokemonSpecies}/></Li></Ul>
+                <Ul>
+                    <Li>Base Stats
+                        <Ul>
+                            {pokemon.stats.map(st => (
+                                <Li key={st.stat.name}>{st.stat.name} - {st.base_stat}</Li>))}
+                        </Ul>
+                    </Li>
+                </Ul>
+                <Ul>
+                    <Li>My Type
+                        <Ul>
+                            {pokemon.types.map(typ => (
+                                <Li key={typ.type.name}>{typ.type.name}</Li>))}
+                        </Ul>
+                    </Li>
+                </Ul>
+                <Ul>
+                    <Li>My Size
+                        <Ul>
+                            <Li>{`weight ${pokemon.weight}`}</Li>
+                            <Li>{`height ${pokemon.height}`}</Li>
+                            <Li>{`order ${pokemon.order}`}</Li>
+                        </Ul>
+                    </Li>
+                </Ul>
+                <Ul>
+                    <Li>My Abilities
+                        <ul>
+                            {pokemon.abilities.map(ability => (
+                                <Li key={ability.ability.name}>{ability.ability.name}</Li>))}
+                        </ul>
+                    </Li>
+                </Ul>
+                <Ul>
+                    <Li>My Moves
+                        <Ul>
+                            {pokemon.moves.map(move => (
+                                <Li key={move.move.name}>
+                                    <Link
+                                        to={`/move/${move.move.name}`}>{move.move.name}</Link>
+                                </Li>))}
+                        </Ul>
+                    </Li>
+                </Ul>
+            </div>
+        </Container>
+    </>);
+
 };
 
 
