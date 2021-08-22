f59bc27b28c9db3284a862d8a56ab47ae3292730
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2021-08-22 15:48:29 +0300

    Pokémon: added RightSidebar component

---
+++ src/Components/Layout/RightSidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NotCapturedPokemon from "../UI/NotCapturedPokemon";
+import CapturedPokemons from "../UI/CapturedPokemons";
+
+const RightSidebar: React.FC = () => {
+    return (
+        <>
+            <NotCapturedPokemon/>
+            <CapturedPokemons/>
+        </>
+    )
+}
+export default RightSidebar;
